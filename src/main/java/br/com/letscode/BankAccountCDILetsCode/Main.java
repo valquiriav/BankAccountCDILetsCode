647509b04ad8bda240890356339743b5a4e4d82b
valquiriav <valvitoli@gmail.com>
2021-08-09 20:39:59 -0300

    Main method

--- src/main/java/br/com/letscode/BankAccountCDILetsCode/Main.java
+++ src/main/java/br/com/letscode/BankAccountCDILetsCode/Main.java
@@ -3,6 +3,8 @@ package br.com.letscode.BankAccountCDILetsCode;
 import org.jboss.weld.environment.se.Weld;
 import org.jboss.weld.environment.se.WeldContainer;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args){
@@ -12,6 +14,24 @@ public class Main {
     }
 
     private static void init(Application application) {
+        int option = 0;
+        Scanner scanner = new Scanner(System.in);
 
+        do {
+            System.out.println("Welcome! Choose an option: \n 1 - Authenticate user \n 2 - Create user \n 0 - Logout");
+            option = scanner.nextInt();
+            switch (option) {
+                case 1:
+                    //authenticateUser();
+                    break;
+                case 2:
+                    //createUser();
+                    break;
+                case 0:
+                    System.exit(0);
+                default:
+                    System.err.println("Invalid option.");
+            }
+        } while (option > 0);
     }
 }
