4c74bc13e11b37f964a684bf8e0fac7a40e36048
valquiriav <valvitoli@gmail.com>
2021-08-09 14:41:09 -0300

    Main method

--- src/main/java/br/com/letscode/BankAccountCDILetsCode/Main.java
+++ src/main/java/br/com/letscode/BankAccountCDILetsCode/Main.java
@@ -1,9 +1,17 @@
 package br.com.letscode.BankAccountCDILetsCode;
 
+import org.jboss.weld.environment.se.Weld;
+import org.jboss.weld.environment.se.WeldContainer;
+
 public class Main {
 
     public static void main(String[] args){
-
+        final WeldContainer container = new Weld().initialize();
+        final Application application = container.select(Application.class).get();
+        init(application);
     }
 
+    private static void init(Application application) {
+
+    }
 }
