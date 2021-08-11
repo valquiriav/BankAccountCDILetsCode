03cd143a60e554712659f3b68392b1ecb4edce8d
valquiriav <valvitoli@gmail.com>
2021-08-11 13:36:57 -0300

    Main

--- src/main/java/br/com/letscode/BankAccountCDILetsCode/Main.java
+++ src/main/java/br/com/letscode/BankAccountCDILetsCode/Main.java
@@ -1,5 +1,7 @@
 package br.com.letscode.BankAccountCDILetsCode;
 
+import br.com.letscode.BankAccountCDILetsCode.Domain.Account;
+import br.com.letscode.BankAccountCDILetsCode.Domain.AccountEnum;
 import br.com.letscode.BankAccountCDILetsCode.Domain.User;
 import org.jboss.weld.environment.se.Weld;
 import org.jboss.weld.environment.se.WeldContainer;
@@ -19,17 +21,28 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         do {
-            System.out.println("Welcome! Choose an option: \n 1 - Authenticate user \n 2 - Create user \n 0 - Logout");
+            System.out.println("Welcome! Choose an option: \n 1 - Create user \n 2 - Create account \n 0 - Logout");
             option = scanner.nextInt();
             switch (option) {
                 case 1:
-                    application.getUserView().authenticate(scanner);
-                    break;
-                case 2:
                     application.createUser(scanner);
                     application.getUsers().stream()
                             .map(User::getFilePath).forEach(System.out::println);
                     break;
+                case 2:
+                    System.out.println("Choose the type of your new account: \n 1 - Savings Account \n 2 - Special Account");
+                    int accOption = scanner.nextInt();
+                    AccountEnum accountEnum = null;
+                        switch (accOption){
+                            case 1:
+                                accountEnum = AccountEnum.SAVINGS;
+                                break;
+                            case 2:
+                                accountEnum = AccountEnum.SPECIAL;
+                                break;
+                        }
+                        application.createAccount(accountEnum, new Account());
+                        break;
                 case 0:
                     System.exit(0);
                 default:
